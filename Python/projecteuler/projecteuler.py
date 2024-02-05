6b97cea8b50b54c3f62f21a05fc3c437365b3d0a
admin <admin@example.com>
2024-02-05 20:32:58 +0000

    add problem1_alternative1

--- Python/projecteuler/projecteuler.py
+++ Python/projecteuler/projecteuler.py
@@ -1,7 +1,17 @@
 def prob1():
-# Find the sum of all the multiples of 3 or 5 below 1000
+    # Find the sum of all the multiples of 3 or 5 below 1000
     sum = 0
     for num in range(1000):
         if num % 3 == 0 or num % 5 == 0:
             sum += num
+    return sum
+
+def prob1_alternative1():
+    def ismult3or5(num):
+        if num % 3 == 0 or num % 5 == 0:
+            return num
+        return 0
+    sum = 0
+    for num in range(1000):
+        sum += ismult3or5(num)
     return sum
