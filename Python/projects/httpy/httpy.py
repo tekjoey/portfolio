662cb120fec04fb49ca3684b50e4d18a537cd0e2
Josiah McKay <josiah.mckay@proton.me>
2024-03-28 08:40:00 -0400

    various edits to become compliant with http/1.1

--- Python/projects/httpy/httpy.py
+++ Python/projects/httpy/httpy.py
@@ -1,19 +1,37 @@
 # A stupid simple unsecure webserver that makes a lot of assumptions and cuts a lot of corners.
 # Seriously, this is not a production application. I'm just using it to learn about sockets, etc.
 # Inspired by Coding a Web Server in 25 Lines - Computerphile -> https://www.youtube.com/watch?v=7GBlCinu9yg
+# SoliDeoGloria
 
-# This project has been created to be a git submodule of the Python/projects directory. I've done this for a few reasons...
-# 1) to learn about submodules
-# 2) To learn about pushing to seperate remotes
-# 3) to learn about using Atlasian products. This project has been pushed to a Bitbucket instance, and I (sorta) use Jira et all to track the tasks for this project.
+import socket, pathlib, argparse, datetime
 
-import socket, pathlib, argparse
+from urllib.parse import urlparse
 
 base_path = pathlib.Path(__file__).parent / 'www'
 
-binary_file_types = ['ico', 'png', 'jpg', 'jpeg']
+# Define parsing characters as per RFC 2616 2.2
+CRLF = '\r\n'       # Carriage return & line feed
+LWS = f'{CRLF} '    # Linear white space
 
-response_404 = """
+http_version = "HTTP/1.1"
+
+binary_files = {
+    '.ico': 'image/vnd.microsoft.icon',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+}
+
+file_types = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript'
+} | binary_files
+
+response_404 = (
+    f'{http_version} 404  Not Found{CRLF}',     #⬅️ Headers
+                                                #⬇️ HTML Response Body; TODO, make this an actual HTML file
+    """
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,6 +43,7 @@ response_404 = """
     404 Page Not Found
 </body>
 </html>"""
+)
 
 def main():
     parser = argparse.ArgumentParser(description='A terrible web server written in Python')
@@ -33,7 +52,7 @@ def main():
     args = vars(parser.parse_args())
     
     listen_port = args['port']
-    listen_address = args['interface']
+    listen_address = "192.168.100.10" #args['interface']
     
     host = (listen_address, listen_port)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -41,46 +60,25 @@ def main():
         s.listen(5)
         print(f"Listening on port {listen_port}")
         print(f"Listening on {'all interfaces' if listen_address == '' else f'interface {listen_address}'}")
+        print(s)
         while True:
+            print("Accepting connections")
             try:
+                print("begin try")
                 conn, addr = s.accept()
+                print("connection accepted!")
                 with conn:
                     print('Connected by', addr)
                     data = conn.recv(1024).decode("utf-8")
-                    headers = parse_headers(data)
-                    if headers['verb'] == "GET":
-                        response_headders = "HTTP/1.1 200 OK\r\n"
-                        location = "index.html" if headers["location"] == '/' else headers["location"][1:]
-                        location_file_type = location.split('.')[-1]
-                        print(f"Location is {location}")
-                        print(f"trying to open {location}")
-                        
-                        match location_file_type:
-                            case binary if binary in binary_file_types:
-                                print("Location is binary file type")
-                                try:
-                                    with open(f"{base_path}/{location}", 'rb') as f:
-                                        print(f"Successfully opened {location}")
-                                        response_content = f.read()
-                                except FileNotFoundError:
-                                    print(f"Failed to open {location}, using 404 default instead")
-                                    response_headders = 'HTTP/1.1 404  Not Found'
-                                    response_content = response_404
-                                conn.send(response_headders.encode("utf-8"))
-                                conn.send('\r\n'.encode("utf-8")) # to separate headers from body
-                                conn.send(response_content)
-                            case _:
-                                print("Location is NOT a binary file type")
-                                try:
-                                    with open(f"{base_path}/{location}", 'r') as f:
-                                        response_content = f.read()
-                                except FileNotFoundError:
-                                    print(f"Failed to open {location}, using 404 default instead")
-                                    response_headders = 'HTTP/1.1 404  Not Found'
-                                    response_content = response_404
-                                conn.send(response_headders.encode("utf-8"))
-                                conn.send('\r\n'.encode("utf-8")) # to separate headers from body
-                                conn.send(response_content.encode("utf-8"))
+                    receive_headers = parse_headers(data)
+                    if receive_headers['verb'] == "GET":
+                        response_headers, response_content = get(receive_headers)
+                        conn.send(response_headers.encode("utf-8"))
+                        print("sent headers")
+                        conn.send(CRLF.encode("utf-8")) # to separate headers from body
+                        print("sent CRLF")
+                        conn.send(response_content)
+                        print("Sent body")
             except KeyboardInterrupt:
                 ConnectionAbortedError
                 s.close()
@@ -88,6 +86,7 @@ def main():
                 
 
 def parse_headers(data: str) -> dict:
+    print(data)
     headers = {}
     list_version = data.split("\r\n")
     verb, location, http = list_version[0].split(" ")
@@ -102,7 +101,56 @@ def parse_headers(data: str) -> dict:
 
         
     # headers[field] = option
+    print(headers)
     return headers
+
+def get(recieve_headers:dict):
+    header_date = "Date: " + datetime.datetime.strftime(datetime.datetime.now(datetime.UTC), '%a, %d %b %Y %H:%M:%S GMT')
+    header_server = "Server: HTTPY/0.1"
+    
+    # HTTP 1.1 requires servers to reject clients who do not use the Host header field
+    if recieve_headers['http version'] == 'HTTP/1.1' and "Host" not in recieve_headers.keys():
+        print(recieve_headers)
+        return f'{http_version} 400 Bad Request{CRLF}{header_date}{CRLF}{header_server}{CRLF}Content-Type: text/html{CRLF}Content-Language: en-US{CRLF}', """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTTPY 400</title>
+</head>
+<body>
+    <h2>No Host: header received</h2>
+    <p>HTTP 1.1 requests must include the Host: header.</p>
+</body></html>
+""".encode('utf-8')
+
+    # HTTP1.1 requires servers to be able to manage full URLs, not just pathnames
+    url = urlparse(recieve_headers['location'])
+    location = "index.html" if url.path == '/' else url.path[1:]
+    location_file_type = pathlib.Path(location).suffixes[-1]
+    
+    print(f"Location is {location}")
+    print(f"Trying to open {location}")
+
+    try:
+        with open(f"{base_path}/{location}", 'rb') as f:
+            print(f"Successfully opened {location}")
+            response_content = f.read()
+            content_size = pathlib.Path(base_path/ location).stat().st_size
+    except FileNotFoundError:
+        print(f"'{location}' not found, using 404 default instead")
+        return response_404
+    
+    if location_file_type not in binary_files:
+        response_content = response_content.decode('utf-8').encode('utf-8')
+
+    header_status = f"{http_version} 200 OK"
+    header_content_type = "Content-Type: " + file_types[location_file_type]
+    #header_content_length = f"Content-Length: {str(content_size + 101)} B"
+    header = f'{header_status}{CRLF}{header_date}{CRLF}{header_server}{CRLF}{header_content_type}{CRLF}Content-Language: en-US{CRLF}'#{header_content_length}'
+
+    return header, response_content
 
 if __name__ == "__main__":
     main()
